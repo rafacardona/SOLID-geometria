42759bde0ab5c19d51b9afc3e514dcfc6799f6d4
rcardona <rcardona@trevenque.es>
2023-03-24 09:40:13 +0100

    Rectangulo cuadrado y circunferencia

--- Geometria.php
+++ Geometria.php
@@ -8,23 +8,68 @@ interface Geometria{
 class Paralelogramo implements Geometria{
     private $base;
     private $altura;
-
     //constructor de paralelogramo
     public function __construct($base, $altura)
     {
         $this->base = $base;
         $this->altura = $altura;
     }
-
     public function area()
     {
-        // TODO: Implement area() method.
         return $this->base * $this->altura;
+    }
+    public function superficie()
+    {
+        return 2 * ($this->base + $this->altura);
+    }
+}
+
+class Rectangulo extends Paralelogramo{
+    public function __construct($base, $altura)
+    {
+        parent::__construct($base, $altura);
+    }
+
+    /*
+     * El area y la superficide de un paralelogramo y un rectangulo se calculan de la misma manera
+     *
+     */
+}
+
+class Cuadrado extends Rectangulo {
+
+    public function __construct($lado)
+    {
+        parent::__construct($lado, $lado);
+    }
+
+    /*
+     * El area seria LADO * LADO y es lo que hace area() de Rectangulo
+     *
+     * La superficie igual, 2(a+b)
+     */
+}
+
+class Circunferncia implements Geometria{
+    //radio de circuferencia
+    private $radio;
+    public function __construct($radio)
+    {
+        $this->radio = $radio;
     }
 
     public function superficie()
     {
-        // TODO: Implement superficie() method.
-        return 2 * ($this->base + $this->altura);
+        //la superficie es 2 * PI * radio
+        return 2 * 3.14 * $this->radio;
+
+    }
+    public function area()
+    {
+    //area es pi por radio al cuadrado
+        return 3.14 * pow($this->radio , 2);
     }
 }
+
+
+
